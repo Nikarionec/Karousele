fad3456ca86b51ab9ee3f8c2333179759c9b3fc2
Nikarionec <126629959+Nikarionec@users.noreply.github.com>
2023-11-20 23:04:14 +0200

    Add files via upload

--- ImageAdapter.java
+++ ImageAdapter.java
@@ -14,8 +14,8 @@ import java.util.List;
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
 
     private Context context;
-    private List<Integer> images;
-    private List<String> descriptions;
+    private List<Integer> images; // Список ідентифікаторів зображень
+    private List<String> descriptions; // Список описів зображень
 
     public ImageAdapter(Context context, List<Integer> images, List<String> descriptions) {
         this.context = context;
@@ -26,14 +26,15 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Створення нового елемента у списку за допомогою макету item_image
         View view = LayoutInflater.from(context).inflate(R.layout.item_image, parent, false);
         return new ImageViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
-        int imageRes = images.get(position);
-        holder.imageView.setImageResource(imageRes);
+        int imageRes = images.get(position); // Отримання ідентифікатора зображення за позицією
+        holder.imageView.setImageResource(imageRes); // Встановлення зображення для ImageView
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,8 +42,8 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
                 int adapterPosition = holder.getAdapterPosition();
                 if (adapterPosition != RecyclerView.NO_POSITION) {
                     String description = descriptions.get(adapterPosition);
-                    // Perform actions with the description (e.g., update a TextView)
-                    // For example: descriptionTextView.setText(description);
+                    // Виконання дій з описом (наприклад, оновлення TextView)
+                    // Наприклад: descriptionTextView.setText(description);
                 }
             }
         });
@@ -50,15 +51,15 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
 
     @Override
     public int getItemCount() {
-        return images.size();
+        return images.size(); // Повернення кількості зображень у списку
     }
 
     public static class ImageViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
+        ImageView imageView; // Об'єкт ImageView для зображення
 
         public ImageViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.imageView);
+            imageView = itemView.findViewById(R.id.imageView); // Знаходження ImageView за його ID
         }
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -11,51 +11,36 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager2 viewPager;
-    private TextView descriptionTextView;
+    private ViewPager2 viewPager; // Об'єкт ViewPager2 для перегляду списку зображень
+    private TextView descriptionTextView; // Об'єкт TextView для відображення опису
 
-    private List<Integer> images = new ArrayList<>();
-    private List<String> descriptions = new ArrayList<>();
+    private List<Integer> images = new ArrayList<>(); // Список ідентифікаторів зображень
+    private List<String> descriptions = new ArrayList<>(); // Список описів зображень
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Встановлення макету для активності
 
-        viewPager = findViewById(R.id.viewPager);
-        descriptionTextView = findViewById(R.id.descriptionTextView);
+        viewPager = findViewById(R.id.viewPager); // Знаходження ViewPager2 за його ID
+        descriptionTextView = findViewById(R.id.descriptionTextView); // Знаходження TextView за його ID
 
+        // Додавання ідентифікаторів зображень у список
         images.add(R.drawable.screenshot1);
         images.add(R.drawable.screenshot2);
-        images.add(R.drawable.screenshot3);
-        images.add(R.drawable.screenshot4);
-        images.add(R.drawable.screenshot5);
-        images.add(R.drawable.screenshot6);
-        images.add(R.drawable.screenshot7);
-        images.add(R.drawable.screenshot8);
-        images.add(R.drawable.screenshot9);
-        images.add(R.drawable.screenshot10);
+        // Додавання описів зображень у список
+        descriptions.add("Nike Air Force 1 Low: Класичні кросівки...");
 
-
-        descriptions.add("Nike Air Force 1 Low: Класичні кросівки з натуральної шкіри, історично популярні та зручні для повсякденного носіння.");
-        descriptions.add("Nike Air Max 270: Сучасні кросівки з максимальним повітряним амортизатором та стильним дизайном для активного способу життя.");
-        descriptions.add("Nike React Infinity Run Flyknit: Легкі та зручні бігові кросівки з інноваційною підошвою React, що забезпечує комфорт та підтримку.");
-        descriptions.add("Nike Zoom Pegasus Turbo 2: Професійні бігові кросівки з високою віддачею енергії та легкістю для швидкого темпу.");
-        descriptions.add("Nike Air Jordan 1: Легендарні кросівки, що стали іконами культури, з унікальним дизайном та відмінною якістю.");
-        descriptions.add("Nike Blazer Mid '77 Vintage: Вишукані кросівки відновленої моделі з ретро-стилем та відмінною комфортністю.");
-        descriptions.add("Nike Air Vapormax Flyknit: Інноваційні кросівки з повітряною підошвою, що забезпечує високу амортизацію та легкість.");
-        descriptions.add("Nike Free RN 5.0: Легкі та гнучкі кросівки для тренувань, що підкреслюють природний рух стопи.");
-        descriptions.add("Nike Air Zoom Structure 24: Кросівки з відмінною стабільністю та підтримкою для тренувань та бігу.");
-        descriptions.add("Nike Joyride Run Flyknit: Інноваційні кросівки з унікальною технологією Joyride для максимальної амортизації та комфорту при кожному кроці.");
-
-
+        // Створення об'єкта ImageAdapter і передача списків зображень та описів
         ImageAdapter adapter = new ImageAdapter(this, images, descriptions);
-        viewPager.setAdapter(adapter);
+        viewPager.setAdapter(adapter); // Встановлення адаптера для ViewPager2
 
+        // Реєстрація колбеку для слідкування за зміною сторінок у ViewPager2
         viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
+                // Отримання опису для поточної сторінки та встановлення його у TextView
                 String description = descriptions.get(position);
                 descriptionTextView.setText(description);
             }
